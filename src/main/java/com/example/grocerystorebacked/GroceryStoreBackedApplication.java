d52073238d466b4d5a13ad5e03531a9dc2966b56
Kunalchaudhari965 <chaudharikunal246@gmail.com>
2025-04-26 17:10:16 +0530

    Added initials

---
+++ src/main/java/com/example/grocerystorebacked/GroceryStoreBackedApplication.java
@@ -0,0 +1,13 @@
+package com.example.grocerystorebacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryStoreBackedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryStoreBackedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grocerystorebacked/GroceryStoreBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grocerystorebacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreBackedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
