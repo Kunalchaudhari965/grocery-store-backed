051dc746c62a0d1c8cf7d32f471e40b475991e25
Piyush(piko04) <156587759+piko4@users.noreply.github.com>
2025-04-27 13:33:30 +0530

    Added cross origin

--- src/main/java/com/example/grocerystorebacked/Controller/UserController.java
+++ src/main/java/com/example/grocerystorebacked/Controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin (origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/user")
 public class UserController
--- src/main/java/com/example/grocerystorebacked/Model/User.java
+++ src/main/java/com/example/grocerystorebacked/Model/User.java
@@ -18,6 +18,7 @@ public class User
     @Column(unique = true)
     private String username;
     private String email;
+    private String mobile;
     private String password;
     private String address;
 
